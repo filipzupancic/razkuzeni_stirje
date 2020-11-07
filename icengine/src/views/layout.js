43ff22a51bd284cadd4184f9ba8a131faff38458
filipzupancic <fz9889@student.uni-lj.si>
2020-11-07 22:17:09 +0100

    Adding cnages

--- icengine/src/views/layout.js
+++ icengine/src/views/layout.js
@@ -29,15 +29,16 @@ class Layout {
 		img_url = 'https://tojesamentest-api.herokuapp.com/get_img?coord='+ bb[2] +'|' + bb[0] + '|' + bb[3]  + '|' + bb[1]  + '&res=50&date_from=2020-4-1&date_to=2020-4-30'
 		m.redraw();
 	}
+
     view() {
         return m("section.u-align-center.u-clearfix.u-image.u-section-1#carousel_5496", [
         	m("div.u-align-center-xs.u-sheet.u-sheet-1", [
         		m("h3.u-align-center-xs.u-text.u-text-1", "SNOW"),
         		m("p.u-text.u-text-2", "OBSERVATION PLATFORM"),
-        		
+
 	        ]),	
 	        m("div.divider", [
-	        	m('div.search', [
+	        	m('div.search_container', [
 	        		m('p', 'Search for location'),
 		        	m('input', {
 		        		onkeyup: e => {
@@ -51,16 +52,16 @@ class Layout {
 
 		        	loading &&
 		        	m('p', 'Loading...'),
-
-		        	api_feed.map(loc => {
-		        		return m('p', {
-		        			style: 'cursor: pointer',
-		        			onclick: e => {
-		        				this.get_img(loc)
-		        			}
-		        		}, loc.display_name)
-		        	}),
-
+		        	m("div.suggestions", [
+			        	api_feed.map(loc => {
+			        		return m('p', {
+			        			style: 'cursor: pointer',
+			        			onclick: e => {
+			        				this.get_img(loc)
+			        			}
+			        		}, loc.display_name)
+			        	}),
+			        ])
 	        	]),
 	        	m('div.image_container', [
 		        	m('img', {
