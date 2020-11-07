1815c2a191d2d0ea1cd5799c15b8451fecb99bd3
mz9337 <zadravecmiha987@gmail.com>
2020-11-07 22:07:44 +0100

    charts

--- icengine/index.js
+++ icengine/index.js
@@ -6,9 +6,11 @@ let port = process.env.PORT || 5000;
 
 let root = path.join(__dirname, '/');
 let bin = path.join(__dirname, 'bin/');
+let public = path.join(__dirname, 'public/');
 
 app.use(express.static(root));
 app.use(express.static(bin));
+app.use(express.static(public));
 
 app.listen(port, function () {
 	console.log("Listening on port: "+port);
--- icengine/src/views/layout.js
+++ icengine/src/views/layout.js
@@ -9,6 +9,124 @@ let typeahead_timeout = null;
 
 let img_url = '';
 
+let by_years = false;
+
+let years = {
+	'2017': [20, 30, 20, 10, 40, 5, 7, 1, 10, 20, 30, 33],
+	'2018': [40, 3, 2, 5, 40, 5, 7, 9, 13, 20, 30, 33],
+	'2019': [20, 10, 40, 20, 30, 1, 10, 5, 7, 20, 30, 33],
+	'2020': [20, 90, 2, 10, 40, 5, 7, 1, 10, 20, 30, 33],
+}
+
+
+// let byYears = {
+// 	'year': [20, 30, 20, 10, 40, 5, 7, 1, 10, 20, 30, 33],
+// 	'2018': [40, 3, 2, 5, 40, 5, 7, 9, 13, 20, 30, 33],
+// 	'2019': [20, 10, 40, 20, 30, 1, 10, 5, 7, 20, 30, 33],
+// 	'2020': [20, 30, 20, 10, 40, 5, 7, 1, 10, 20, 30, 33],
+// }
+
+class Chart {
+	oncreate(vnode) {
+		// var ctx = vnode.dom.querySelector('canvas').getContext('2d');
+		// this.ctx = ctx;
+
+		var y_data = Object.keys(years).map(year => {
+        	return [year].concat(years[year]);
+        });
+
+        var y_types = {};
+
+        Object.keys(years).forEach(year => {
+        	y_types[year] = 'bar';
+        });
+
+		// console.log(y_types);
+
+		// if (!by_years) {
+		var chart = c3.generate({
+		    bindto: '#chart',
+		    data: {
+		        columns: y_data,
+		        types: y_types,
+		    },
+	        axis: {
+		        y: {
+		            max: 100,
+		            min: 10,
+		        },
+		        x: {
+			        type: 'category',
+			        categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+			    }
+		    }
+		});
+		// } 
+
+		var y_data_by_year = [];
+		var y_labels_by_year = Object.keys(years);
+
+		Object.keys(years).forEach(year => {
+        	y_data_by_year.push(Math.round((years[year].reduce((t, n) => t+n, 0)/years[year].length)*100) / 100);
+        });
+
+		// console.log(y_data_by_year);
+		// console.log(['year'].concat(y_data_by_year),);
+
+		// if (by_years) {
+		var chartByYears = c3.generate({
+		    bindto: '#chartByYears',
+		    data: {
+		        columns: [['year'].concat(y_data_by_year)],
+		        types: {
+		        	year: 'bar'
+		        },
+		    },
+	        axis: {
+		        y: {
+		            max: 100,
+		            min: 10,
+		        },
+		        x: {
+			        type: 'category',
+			        categories: y_labels_by_year
+			    }
+		    }
+		});
+		// }
+	}
+
+	view() {
+		return m('div.graph-box', [
+			m('div.select-graph-type', [
+				m('div' + (!by_years ? '.selected' : ''), {
+					onclick: e => {
+						by_years = false;
+						m.redraw();
+					},
+				}, 'Over year'),
+
+				m('div' + (by_years ? '.selected' : ''), {
+					onclick: e => {
+						by_years = true;
+						m.redraw();
+					}
+				}, 'All years')
+			]),
+
+			m('div', {style: 'display: ' + (!by_years ? 'block;' : 'none;') }, [
+				m('div#chart'),
+			]),
+			m('div', {style: 'display: ' + (by_years ? 'block;' : 'none;') }, [
+				m('div#chartByYears'),
+			])
+			// m('btn', {
+			// 	onclick: e => this.init_graph()
+			// }, 'ee')
+		])
+	}
+}
+
 class Layout {
 	get_list(query) {
 		loading = true;
@@ -67,7 +185,8 @@ class Layout {
 		        		src: img_url
 		        	})
 	        	])
-	        ])
+	        ]),
+	        m(Chart)
 	    ])
     }
 }
