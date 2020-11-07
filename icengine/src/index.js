8913acda49f623cd85900dafbf88d7e82890031d
filipzupancic <fz9889@student.uni-lj.si>
2020-11-07 10:55:33 +0100

    Adding some changes

--- icengine/src/index.js
+++ icengine/src/index.js
@@ -1,25 +1,5 @@
 import m from "mithril";
 import Home from "./home"
 
-let show_text=false;
-
 // m.mount(document.body, Home);
-
-class Test {
-	view() {
-		return m('div.container', [
-					m('h1', 'ICENGINE'),
-						m('input', {type:'number'}),
-
-						m('button.to-je-c', {
-							onclick: e => {
-							show_text=true;
-						} 
-				}, 'gumb'),
-
-				show_text && m('p', 'to_e en text')
-		])
-	}
-}
-
-m.mount(document.body, Test);
+m.mount(document.body, Home);
